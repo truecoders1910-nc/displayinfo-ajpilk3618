f78c6e9db29b444b277ac770923a700e529dcd05
AJ <apilkerton3618@gmail.com>
2019-10-25 19:17:26 -0500

    first half of project completed

--- DisplayInfo/Program.cs
+++ DisplayInfo/Program.cs
@@ -7,14 +7,25 @@ namespace DisplayInfo
         static void Main(string[] args)
         {
             // Get first and last name from user input
+            Console.WriteLine("Please enter your first name.");
+            string Firstname = Console.ReadLine();
+            Console.WriteLine("Please enter your last name.");
+            string Lastname = Console.ReadLine();
+
             // Get their favorite number
+            Console.WriteLine($"Hello {Firstname} {Lastname}, please enter your favorite number.");
+            string Favenum = Console.ReadLine();
+            int FavenumValue = Convert.ToInt32(Favenum); 
+           
             // Get their favorite animal
+
 
             // Once you have gotten all their info and stored it,
             // display it back to them.
 
             // If their favorite number is greater than 10, display it in green.
             // If their favorite number is less than 5, display it in red.
+            Console.ReadLine();
         }
     }
 }
